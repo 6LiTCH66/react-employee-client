54b4abda0855b337251898ccadb15f9b0c0756ea
Ilja H <ilja200303@gmail.com>
2022-04-01 12:07:59 +0300

    make it better

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express();
+
+const path = require('path')
+const PORT = process.env.PORT || 6000
+
+app.use(express.static('build'));
+app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', "index.html"));
+})
+
+app.listen(PORT, (err) => {
+    if (err) return console.log(err)
+    console.log(`Server running on PORT:`, PORT)
+})
+
