9455e465ed25856d4ace76898592204ed7782973
Ilja H <ilja200303@gmail.com>
2022-04-04 12:34:59 +0300

    node.js server fix

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
+
 var express = require('express')
 var app = express();
 
 const path = require('path')
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 8080
 
-app.use(express.static('build'));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', "/index.html"));
-})
+var app_path = '../build'
 
-app.listen(PORT, (err) => {
-    if (err) return console.log(err)
-    console.log(`Server running on PORT:`, PORT)
-})
+app.use('/', express.static(path.join(__dirname, app_path)))
+    .get('*', (req, res)=>res.sendFile(path.join(__dirname, app_path + '/index.html'))).listen(PORT, ()=> console.log(`Listening on ${PORT}`))
 
